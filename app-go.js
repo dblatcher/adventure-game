5a82552a78be12b494ab33e29eb01d9debbabd2c
dblatcher <dv.bltchr@gmail.com>
2019-05-25 18:42:05 +0100

    make stuff better, like putting in the action methods and stuff. started on the move method, might break the thing, dunno

--- app-go.js
+++ app-go.js
@@ -4,41 +4,58 @@ var vm = new Vue({
   data: {
     sprites : [
       {id:0, url: 'boy.png', row:4, col:4},
-      {id:'m', url: 'mario.png', row:2, col:3},
       {id:2, url: 'boy2.png', row:4, col:4},
+      {id:'m', url: 'mario.png', row:2, col:3},
     ],
 	characters : [
 		{id:'npc', name:'Billy', speechColor: 'red',
-		spritesUsed:[2,0], initialFrame:"0,0",
+		spritesUsed:[2,0],
+		validDirections : ['down','left','right','up'],
 		baseHeight:50,baseWidth:50,
 		startX:10,startY:150,
 		cycles : {
+			wait: [[0,0,0]],
 			talk: [[0,0,0],[2,0,0]],
 			walk: [[0,0,1],[0,1,1],[0,2,1],[0,3,1]  ]
-		}},
+		},
+		},
 		
 		{id:'pc', name:'Mario', speechColor: 'blue', 
 		spritesUsed:['m'],
+		validDirections : ['right','left'],
 		baseHeight:75,baseWidth:50,
 		startX:20,startY:300,
 		cycles : {
-			walk: [ ['m',0,0],['m',1,0],['m',2,0]  ]
-		}}
-	]
+			wait: {
+				right: [ ['m',0,0]  ],
+				left: [ ['m',2,1] ],
+			},
+			walk: {
+				right:[ ['m',0,0],['m',1,0],['m',2,0]  ],
+				left: [ ['m',2,1],['m',1,1],['m',0,1]  ]
+			}
+		},
+		}
+	],
+	message: 'blank message'
+  },
+  methods : {
+	logEvent: function(event) {
+		console.log(event);
+	}  
   },
 
   mounted: function () {
-  
+	this.$on('get-report',function(component,data){
+		this.message = `message from ${component.name}: ${data}.`
+	})
   }
 
 })
 
 var pc = getChildByIdent('pc');
 var npc = getChildByIdent('npc');
-pc.say('Hi');
-pc.say('I am '+pc.char.name);
-pc.say('I have some dialogue');
-pc.say('I have done.');
+
 
 function getChildByIdent (ident,instance = vm) {
   var rightChild = null ;
--- character-comp.js
+++ character-comp.js
@@ -6,60 +6,87 @@ Vue.component('character-c', {
 		var spriteSet = [];		
 		var fullSet = this.$parent.$data.sprites;
 		for (var i=0; i< fullSet.length; i++) {		
-			if (this.char.spritesUsed.includes(fullSet[i].id)) {
-				spriteSet.push ( Object.assign({}, fullSet[i], {p:this} ) )	
-			}
+			if (this.char.spritesUsed.includes(fullSet[i].id)) {spriteSet.push ( Object.assign({}, fullSet[i], {p:this} ) )	}
 		};
-		return{
+		
+		return {
 		spriteSet : spriteSet,
-		spriteID: spriteSet[0].id,
-		saying:'', sayingQueue :[],
-		
-		action:'walk', actFrame:0,
-		
 		ident: this.char.id,
 		name: this.char.name,
 		x: this.char.startX ? Number (this.char.startX) : 0, 
 		y: this.char.startY ? Number (this.char.startY) : 0,
-		fx: this.char.initialFrame ? Number(this.char.initialFrame.split(',')[0]) : 0,
-		fy: this.char.initialFrame ? Number(this.char.initialFrame.split(',')[1]) : 0,
 		scale:this.char.initialScale || 1,
 		baseHeight: this.char.baseHeight || 100,
 		baseWidth: this.char.baseWidth || 100,
+		saying:'', sayingQueue :[],
+		action:'wait', actFrame:0, actionOnLoop:true,
+		direction:this.char.validDirections[0],
+		destinationQueue:[]
 		}
 	},
 	computed :{
 		scaledHeight : function() {return this.scale * this.baseHeight;},
 		scaledWidth : function() {return this.scale * this.baseWidth;},
-		currentSprite : function() {
-			for (var i=0; i< this.spriteSet.length; i++) {
-				if (this.spriteSet[i].id === this.spriteID) {
-					return this.spriteSet[i];
-				}
-			}
-			console.error(`character ${this.name} spriteID (${this.spriteID}) is not the id from its spriteSet`);
-			return null;
-		},
 		isTalking : {
 			get: function() {return this.saying !== ''},
 			set: function(v) {if (v===false){this.saying=''; this.sayingQueue=[]}}
+		},
+		frame : function() {			
+			var directionNeeded = !Array.isArray(this.char.cycles[this.action]);
+			var v = directionNeeded ? 
+				this.char.cycles[this.action][this.direction][this.actFrame]:
+				this.char.cycles[this.action][this.actFrame];
+			
+			return {sprite: v[0], fx:v[1], fy:v[2]}
 		}
 	},
 	methods : {
-		setCycleFrame : function (cycle, n=0) {
-			if (!this.char.cycles[cycle]) {
-				console.error(`character ${this.name} has no cycle called ${cycle}`);
-				return false;
-			};
-			if (this.char.cycles[cycle].length <= n) {
-				console.error(`character ${this.name}\'s cycle ${cycle} does not have a frame ${n}`);
-				return false;
+		move : function () {
+			if (this.destinationQueue.length === 0) {return false};
+			
+			var d = {x: this.destinationQueue[0].x - this.x, y:this.destinationQueue[0].y - this.y};
+			//console.log(d);
+			var speed = 5;
+			
+			if (d.x === 0) {
+				
+			} 
+			
+			
+		},
+		act : function (action, options = {}, callBack) {
+			if (typeof action  !== 'string') {return false;}
+			if (!this.char.cycles[action]) {return false;}
+			
+			if (options.direction) {
+				if (this.char.validDirections.includes(options.direction)) {
+					this.direction = options.direction;
+				}
 			};
 			
-			var v = this.char.cycles[cycle][n];			
-			this.spriteID = v[0];
-			this.fx=v[1];
-			this.fy=v[2];
+			var directionNeeded = !Array.isArray(this.char.cycles[action]);		
+			if (directionNeeded) {
+				var availableDirections = Object.keys(this.char.cycles[action]);				
+				this.direction = availableDirections.includes(this.direction) ? this.direction : availableDirections[0];
+			}
+			
+			this.actionOnLoop = options.loop || false;
+			this.action = action; this.actFrame = 0;
+		},
+		showNextFrame : function () {
+			var directionNeeded = !Array.isArray(this.char.cycles[this.action]);
+			var cycle = directionNeeded ? 
+				this.char.cycles[this.action][this.direction] :
+				this.char.cycles[this.action] ;
+
+			var onLastFrame = !(cycle.length > this.actFrame+1);
+			this.actFrame = onLastFrame ? 0: this.actFrame + 1;
+			
+			if (onLastFrame && !this.actionOnLoop) {
+				this.$root.$emit('get-report',this,'last frame');
+				this.action = 'wait';
+				this.actionOnLoop = true;
+			}
 		},
 		say : function (text, time) {
 			if (typeof time !== 'number') {time = 1000}
@@ -83,6 +110,15 @@ Vue.component('character-c', {
 			};
 		}
 	},
+	mounted : function() {
+		this.say('Hello world, I am '+this.name+'.')
+		
+		var that = this;
+		setInterval (function(){
+			that.showNextFrame();
+			that.move();
+		},250);
+	},
 	template: `
 	
 	<article>
@@ -94,11 +130,11 @@ Vue.component('character-c', {
 			left: x+'px'}">
 			<sprite-image v-for="sprite in this.spriteSet":key="sprite.id"
 				v-bind:sprite="sprite"
-				v-bind:fx="sprite.p.fx" 
-				v-bind:fy="sprite.p.fy"
+				v-bind:fx="sprite.p.frame.fx" 
+				v-bind:fy="sprite.p.frame.fy"
 				v-bind:height="sprite.p.scaledHeight"
 				v-bind:width="sprite.p.scaledWidth"
-				v-bind:index="sprite.p.currentSprite.id"
+				v-bind:index="sprite.p.frame.sprite"
 				></sprite-image>
 		</div>
 		
