8eaa7e616c0d613adb6d7da2e83366b1910daafb
dblatcher <dv.bltchr@gmail.com>
2019-05-27 09:39:07 +0100

    new character, fix zIndex of characters based on y coord, mkae diaogue not go off left edge of room

--- app-go.js
+++ app-go.js
@@ -6,9 +6,10 @@ var vm = new Vue({
       {id:0, url: 'boy.png', row:4, col:4},
       {id:2, url: 'boy2.png', row:4, col:4},
       {id:'m', url: 'mario.png', row:2, col:3},
+      {id:'w', url: 'woman.png', row:4, col:9},
     ],
 	characters : [
-		{id:'pc', name:'Billy', speechColor: 'red',
+		{id:'Billy', name:'Billy', speechColor: 'red',
 		spritesUsed:[2,0],
 		validDirections : ['down','left','right','up'],
 		baseHeight:50,baseWidth:50,
@@ -39,7 +40,29 @@ var vm = new Vue({
 				right:[ ['m',0,0],['m',1,0],['m',2,0]  ],
 				left: [ ['m',2,1],['m',1,1],['m',0,1]  ]
 			}
+		}
 		},
+		
+		{id:'pc', name:'Jane', speechColor: 'white',
+		spritesUsed:['w'],
+		validDirections : ['down','left','right','up'],
+		baseHeight:75,baseWidth:50,
+		startX:120,startY:30,
+		cycles : {
+			wait : {
+				up 	  : [ ['w',0,0] ],
+				left  : [ ['w',0,1] ],
+				down  : [ ['w',0,2] ],
+				right : [ ['w',0,3] ],
+			},
+			walk : {
+				up 	  : [ ['w',0,0],['w',1,0],['w',2,0],['w',3,0] ],
+				left  : [ ['w',0,1],['w',1,1],['w',2,1],['w',3,1], ['w',4,1],['w',5,1],['w',6,1],['w',7,1], ['w',8,1] ],
+				down  : [ ['w',0,2],['w',1,2],['w',2,2],['w',3,2] ],
+				right : [ ['w',0,3],['w',1,3],['w',2,3],['w',3,3], ['w',4,3],['w',5,3],['w',6,3],['w',7,3], ['w',8,3] ] ,
+				
+			},
+		}
 		}
 	],
 	rooms: [
@@ -54,12 +77,11 @@ var vm = new Vue({
   },
 
   mounted: function () {
-
 	var room = this.$children[0];  
 	var pc = getChildByIdent('pc',room);
-	pc.say('Hello, World. I am the player character.')
 	
-	marchRightThenLeft( getChildByIdent('npc',room) );
+	pc.say('Hello, World. I am the player character.')
+	//marchRightThenLeft( getChildByIdent('npc',room) );
 	 
 	this.$on('get-report',function(component,data){
 		var now = new Date();
@@ -69,7 +91,6 @@ var vm = new Vue({
 	this.$on('clicked', function (component,type,event){
 		
 		if (type === 'room') {
-			
 			//TO DO -  generate path of steps to navigate around obsticles to closest valid point  
 			var order = {
 				y: (event.target.offsetHeight - event.clientY + event.target.offsetTop),
@@ -85,10 +106,6 @@ var vm = new Vue({
 				(pc.char.validDirections.includes(vertical) ? vertical : horizontal );
 			
 			order.direction = direction;
-			
-			
-			
-			console.log(order);
 			pc.destinationQueue = [order];
 		}
 		
--- speech-line-comp.js
+++ speech-line-comp.js
@@ -1,13 +1,23 @@
 Vue.component('speech-line', {
-	props: ['color','text'],
+	props: ['color','text','charCentre'],
 	computed: {
 		styleObject : function() {
+			var that = this;
 			return {
 				color:this.color,
 				fontWeight:'600',
 				margin: '0',
 				backgroundColor: 'rgba(0,0,0,.25)',
-				zIndex:2
+				transform: function() {
+					var lineWidth = that.text.length * (7.15); // guess at font width in px
+					var shiftLeft = -50;
+					if (that.charCentre < (lineWidth/2)) { 
+						shiftLeft = -50 * (that.charCentre/(lineWidth/2));  
+					}
+					return `translateX(${shiftLeft}%)`;
+				}(),
+				zIndex:2000,
+				fontFamily:'monospace'
 			}
 		}
 	},
