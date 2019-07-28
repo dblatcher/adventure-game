2a83be87504cfc6a7d2c432dc842009a31316088
dblatcher <dv.bltchr@gmail.com>
2019-07-28 14:17:37 +0100

    make the lake room same height as other rooms

--- webventure/src/modules/game-data.js
+++ webventure/src/modules/game-data.js
@@ -107,16 +107,16 @@ var makeCharacters = function() {return [
 
 var makeRooms = function(){ return [
 	
-	new Room ('swamp','swamp',"bg1.png", 400, 300, {
+	new Room ('swamp','swamp',"bg1.png", 400, 250, {
 	worldItems:[
-		new WorldItem ('lake','lake',[200,45],400,50),
+		new WorldItem ('lake','lake',[200,40,20,-20],400,50),
 		new WorldItem ('house','path back to house',[375,0],50,150),
-		new WorldItem ('bucket','bucket',[250,25],40,40,'neutral',worldItemModels.bucket),
-		new WorldItem ('bucket2','big bucket',[100,25],40,40,'neutral',worldItemModels.bucket,1.5),
-		new WorldItem ('fire','fire',[145,30,20,0],40,40,'burning',worldItemModels.fire),
+		new WorldItem ('bucket','bucket',[250,20],40,40,'neutral',worldItemModels.bucket),
+		new WorldItem ('bucket2','big bucket',[80,20],40,40,'neutral',worldItemModels.bucket,1.5),
+		new WorldItem ('fire','fire',[145,20,20,0],40,40,'burning',worldItemModels.fire),
 	],
 	obstacles:[
-		new RectZone (200,45,400,50,true)
+		new RectZone (200,40,400,50,true)
 	]}),
 	 
 	new Room ('LIVING_ROOM', 'Living room', "bg2.jpg", 400, 250,{
