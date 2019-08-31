17aa0c67d18e6eff15661f0a30d52506756bf279
dblatcher <dv.bltchr@gmail.com>
2019-08-31 14:29:17 +0100

    bucket interactions

--- src/steamed-hams/game-data.js
+++ src/steamed-hams/game-data.js
@@ -89,7 +89,7 @@ var worldItemModels = {
 
 
 var makeCharacters = function() {return [
-	new Character ('skinner','Skinner',[200,10,1],'white',characterModels.skinner),
+	new Character ('skinner','Skinner',[200,10,0],'white',characterModels.skinner),
 	new Character ('chalmers','Superintendent Chalmers',[100,10,1],'red',characterModels.chalmers),
 ]}
 
@@ -106,6 +106,7 @@ var makeRooms = function(){ return [
 		],
 		worldItems: [
 			new WorldItem('garage','car hole',[65,16,0,-10],200,170),
+			new WorldItem('bush','bush',[185,14,20,-2],130,65),
 			new WorldItem('front_door','door',[142,28,5,-10],80,120,'closed',worldItemModels.door),
 			new WorldItem('front_fence','f',[155,8],470,129,'neutral',worldItemModels.front_fence,{unclickable:true,zAdjust:10}),
 		],
@@ -164,8 +165,10 @@ var verbList = [
 
 var makeInventoryItems = function() { return  [
 	new InventoryItem('roast', 'raw roast', 'roast.png',true),
-	new InventoryItem('bucket', 'bucket', 'bucket.png'),
-	new InventoryItem('sock', 'sock', 'sock.png'),
+	new InventoryItem('bucket_foil', 'ice bucket', 'bucket_foil.png'),
+	new InventoryItem('bucket_sand', 'fire bucket', 'bucket_sand.png'),
+	new InventoryItem('bucket_empty', 'fire bucket', 'bucket.png'),
+	new InventoryItem('foil', 'aluminium foil', 'sock.png'),
 	new InventoryItem('nail', 'nail', {1:'nail.png',2:'twonails.png',3:'threenails.png',4:'manynails.jpg'},false, {quantity: 2, pluralName: 'nails'}),
 	new InventoryItem('hammer','hammer', 'hammer.jpg'),
 ]};
--- src/steamed-hams/game-interactions.js
+++ src/steamed-hams/game-interactions.js
@@ -4,6 +4,41 @@ import { Interaction, doorFunction,pcSays } from "../modules/interaction-constru
 var interactions =[
 	new Interaction(['LOOK','GARAGE_W'],[],pcSays('I admire car owners. I aspire to be one after I\'ve reimbursed mother for the food I ate as a child.',2500)),
 	new Interaction(['LOOK','ROAST_I'],[],pcSays('Yes, this should be a reasonable quantity of meat to serve Superintendent Chalmers.',2000)),
+	new Interaction(['LOOK','BUCKET_FOIL_I'],[],pcSays('This should suffice. I can hardly see the foil',2000)),
+	new Interaction(['LOOK','BUCKET_SAND_I'],[],pcSays('Hmmm, this could serve as an ice bucket if it were empty and silver colored.',2000)),
+	new Interaction(['LOOK','BUCKET_EMPTY_I'],[],pcSays('If only I could change it\'s color somehow.',2000)),
+	new Interaction(['LOOK','FOIL_I'],[],pcSays('Shiny, metallic and used for wrapping things.',2000)),
+
+	new Interaction(['USE','FOIL_I','BUCKET_SAND_I'],[],pcSays('I need to get rid of this sand first.',2000)),
+	new Interaction(['USE','FOIL_I','BUCKET_FOIL_I'],[],pcSays('It\s already wrapped.',1500)),
+	
+	new Interaction(['USE','BUCKET_SAND_I','BUSH_W'],[],function(){
+		let pc = this.getThings('pc');
+		this.getInventoryItem('BUCKET_EMPTY_I');
+		this.looseInventoryItem('BUCKET_SAND_I');
+		this.setGameStatus('CUTSCENE');
+		let game=this;
+
+		pc.goTo(this.getThings('BUSH_W').walkToPoint)
+		.then( (r) => {return pc.doAction('pour_sand')})
+		.then( (r) => {return pc.say('There!') } )
+		.then( (r) => {return pc.say('I suppose its wrong to use fire-fighting equipment improperly...') } )
+		.then( (r) => {return pc.say('But what are the chances of a fire in the next half hour?',{action:'ponder'}) } )
+		.then( (r) => { game.setGameStatus('LIVE') })
+
+	}),
+
+	new Interaction(['USE','FOIL_I','BUCKET_EMPTY_I'],[],function(){
+		let pc = this.getThings('pc');
+		this.getInventoryItem('BUCKET_FOIL_I');
+		this.looseInventoryItem('BUCKET_EMPTY_I');
+
+		pc.doAction('pour_sand')
+		.then( (r) => {return pc.say('There!') } )
+		.then( (r) => {return pc.say('It looks like a real ice bucket.') } )
+
+	}),
+
 
 
 	new Interaction(['OPEN','FRONT_DOOR_W'],[
