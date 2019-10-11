c6c084a460bb7cde27d37ac1501c516f0c23fb4d
dblatcher <dv.bltchr@gmail.com>
2019-10-11 06:39:24 +0100

    extend chainPromises to handle simultaneous steps in array

--- src/modules/chainPromises.js
+++ src/modules/chainPromises.js
@@ -2,19 +2,30 @@ export default function makeChain(orders, execute, that) {
 
     function doStep(n, resolve, reject) {
 
-        execute.apply (that, [orders[n]])
-        .then(() => {
+        const nextOrEnd = () => {
             if (n+1 >= orders.length) {
                 resolve (true);
                 return;
             }
             doStep (n+1, resolve, reject)
-        })
-        .catch((e) => {
-            console.warn('chain failure',e);
+        }
+        const logRejections = (e) => {
+            console.error('chain failure',e);
             reject (e)
-        })
+        }
 
+        if (Array.isArray(orders[n]) ) {
+            let promiseSubset = orders[n].map( function(order) {
+                return execute.apply (that, [order])
+            } )
+            Promise.all(promiseSubset)
+            .catch(logRejections)
+            .finally(nextOrEnd)
+        } else {
+            execute.apply (that, [orders[n]])
+            .catch(logRejections)
+            .finally(nextOrEnd)
+        }
     }
 
     return new Promise(function (resolve, reject) {
