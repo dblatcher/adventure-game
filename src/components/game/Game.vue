bcf6c2ed3b7e26a85f6ac136fd93d11dfc228015
dblatcher <dv.bltchr@gmail.com>
2019-11-22 00:36:19 +0000

    hide debug messages again

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -89,7 +89,7 @@
       PAUSED
     </aside>
 
-    <p style="display:block; position:absolute; bottom:0; right:0; background-color:white;">
+    <p style="display:none; position:absolute; bottom:0; right:0; background-color:white;">
       <span>{{message}}</span>
       <span ref="coordinateDisplay"></span>
     </p>
