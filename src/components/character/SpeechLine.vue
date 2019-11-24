a24352abcea0111bcc28d323296d476f50053db1
dblatcher <dv.bltchr@gmail.com>
2019-11-24 22:56:24 +0000

    improve speechlines... again

--- src/components/character/SpeechLine.vue
+++ src/components/character/SpeechLine.vue
@@ -1,6 +1,6 @@
 <template>
-	<p v-bind:style="styleObject">
-		<span class="tail" v-bind:style="tailStyleObject"></span>
+	<p v-bind:style="style.box">
+		<span class="tail" v-bind:style="style.tail"></span>
 		<span>{{text}}</span>
 	</p>
 </template>
@@ -10,42 +10,38 @@ export default {
 	name:'SpeechLine',
 	props: ['color','text','measure','pos'],
 	computed: {
-		styleObject : function() {
+		style : function() {
 			const {x,y,characterHeight, characterWidth,roomHeight, roomWidth} = this.pos;
 			const {unit, scale} = this.measure
+			const {text, color} = this
+			const onRight = (x > roomWidth/2);
 			const toCssValue = (v) => ((v*scale)+unit)
 
-			const onRight = (x > roomWidth/2);
-			const left  = onRight  ? 'unset' : toCssValue (x + characterWidth/2)
-			const right = !onRight ? 'unset' : toCssValue (roomWidth - x + characterWidth/2)
-			const top   = toCssValue (Math.max((roomHeight - y - characterHeight),0))
-			const padding  = this.text === '' ? '0': '.5em'
-			const marginLeft  = !onRight ? '0' : '.5rem'
-			const marginRight = onRight  ? '0' : '.5rem'
+			function tail() {return {
+				display: text === '' ? 'none' : 'inline-block',
+				borderLeft:  onRight ? 'unset' : "2rem solid transparent",
+				borderRight:  !onRight ? 'unset' : "2rem solid transparent",
+				left  : onRight  ? 'unset' : '-2rem',
+			 	right : !onRight ? 'unset' : '-2rem',
+			} }
+
+			function box () {return {
+				color: color,
+				display: text === '' ? 'none' : 'flex',
+				left: onRight  ? 'unset' : toCssValue (x + characterWidth/2) ,
+				right: !onRight ? 'unset' : toCssValue (roomWidth - x + characterWidth/2),
+				top: toCssValue (Math.max((roomHeight - y - characterHeight),0)), 
+				marginLeft:  !onRight ? '0' : '.5rem', 
+				marginRight:  onRight  ? '0' : '.5rem',
+			}}
 
 			return {
-				color:this.color,
-				padding,left,right,top, marginLeft, marginRight
+				tail : tail(),
+				box: box(),
 			}
-		},
-		tailStyleObject : function() {
-			const {x,y,characterHeight, characterWidth,roomHeight, roomWidth} = this.pos;
-			const {unit, scale} = this.measure
-			const toCssValue = (v) => ((v*scale)+unit)
 
-			const onRight = (x > roomWidth/2);
-			const left  = onRight  ? 'unset' : '-1rem'
-			const right = !onRight ? 'unset' : '-1rem'
-			const display = this.text === '' ? 'none' : 'inline-block'
-			const borderTopLeftRadius   = !onRight ? '.5rem' : '0'
-			const borderTopRightRadius  =  onRight ? '.5rem' : '0'
-			const borderBottomLeftRadius   = !onRight ? '.5rem' : '0'
-			const borderBottomRightRadius  =  onRight ? '.5rem' : '0'
-
-			return {left, right, display, 
-			borderTopLeftRadius, borderTopRightRadius,
-			borderBottomLeftRadius, borderBottomRightRadius}
 		}
+
 	},
 }
 </script>
@@ -54,24 +50,21 @@ export default {
 	p {
 		position: absolute;
 		font-family: monospace;
-		font-weight: 600;
+		font-weight: 100;
 		background-color: rgba(0,0,0,.6);
 		z-index: 200;
 		margin: 0;
 		border-radius: .5rem;
 		max-width: 50%;
 		min-height: 3rem;
-		display: flex;
+		padding: .5rem;
 		align-items: center;
 	}
 	.tail {
-		position: relative;
 		position: absolute;
-		width: 1rem;
-		height: .5rem;
-		background-color: rgba(0,0,0,.6);
-		bottom:50%;
-		transform: (translateY(50%));
+		bottom: .5rem;
+		border-bottom: 1rem solid  rgba(0,0,0,.6);
+		width: 0rem;
 
 	}
 </style>
