9e34f58577baad4fbbbce5d132ad73d6a628f532
David Blatcher <david.blatcher@graduate-jobs.com>
2020-01-01 10:50:44 +0000

    remove test commit

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,6 @@ export default {
     Game, TitleScreen, EndingScreen, FileMenu, LoadingBar
   },
 
-// comment for test commit
   data () {
     let i, jsonString, savedGames = [];
     for (i=0; i<5; i++) {
