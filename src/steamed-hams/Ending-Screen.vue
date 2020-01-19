e2a5f20b82fd9ccd1a461997f75bc7571bf77537
dblatcher <dv.bltchr@gmail.com>
2020-01-19 21:35:18 +0000

    improve ending screen

--- src/steamed-hams/Ending-Screen.vue
+++ src/steamed-hams/Ending-Screen.vue
@@ -1,22 +1,28 @@
 <template>
     <main class="ending-page">
-        <img class="ending-page__picture" src="./thumbsup.jpg"/>
+
+        <div class="ending-page__picture-frame">
+            <figure class="ending-page__picture"></figure>
+            <h1 class="ending-page__title">Game Over...</h1>
+        </div>
 
         <figure class="ending-page__sprite">
             <Sprite 
             v-bind:sprite="this.chalmers"
             v-bind:fx="this.chalmersFx"
             v-bind:fy=1
-            v-bind:height=16
+            v-bind:height=20
             v-bind:width=10
             v-bind:index="this.chalmers.id"
             v-bind:measure = "{scale:1, unit:'em'}"/>
         </figure>
 
-        <h1 class="ending-page__title">Game Over...</h1>
+
 
         <div class="ending-page__frame">
-            <div class="ending-page__button-set"><slot name="file-buttons"></slot></div>
+            <div class="ending-page__button-set">
+                <slot name="file-buttons"/>
+            </div>
         </div>
 
     </main>
@@ -27,13 +33,14 @@
 import Sprite from "../components/Sprite";
 import {sprites} from "./game-data";
 
+const chalmersSprite = sprites.filter(sprite => sprite.id === 'ch-r')[0]
 
 export default {
     name: "EndingScreen",
     components: {Sprite}, 
     data: function () {
         return {
-            chalmers: sprites[14],
+            chalmers: chalmersSprite,
             chalmersFx: 3,
         }
     }
@@ -46,13 +53,13 @@ export default {
 
 @import "../modules/_layout.scss";
 @import "../modules/_material.scss";
-
+@import url('https://fonts.googleapis.com/css?family=Knewave|Schoolbell&display=swap');
 
 .ending-page {
 
     @include fullscreen;
 
-    background-image: linear-gradient(tan, darkgreen);
+    background-image: url(./thatch.png);
     padding: 1rem;
 
     display: flex;
@@ -64,18 +71,6 @@ export default {
         display: flex;
         flex-flow: column nowrap;
         justify-content: space-around;
-    }
-
-    &__title {
-       @include centerPoint;
-       @include placeAbsolute(50%, 60%, false, false);
-       color: purple;
-       font-family: cursive;
-       text-shadow: 0px 1px black;
-       font-size: 2.5rem;
-       line-height: 3.25rem;
-       text-align: center;
-       white-space: nowrap;
     }
 
     &__button-set {
@@ -91,24 +86,64 @@ export default {
         @include placeAbsolute(5%, 1rem, false, true);
     }
 
-    &__picture {
+    &__picture-frame {
         @include centerPoint;
         @include placeAbsolute(50%, 50%, false, false);
+
         border-radius: 50%;
-        box-shadow: $drop-shadow1;
-        z-index: -1;
-        filter: sepia(1);
+        box-sizing: border-box;
+        border: 15px ridge #bc6421;
+        border-style: ridge;
+        box-shadow: $drop-shadow2;
+        padding:0;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
         @media (orientation: portrait) {
-            width: 90%;
-            max-width: 600px;
-            
+            width: 84vw;
+            height: 72vw;
         }
 
         @media (orientation: landscape) {
-            height: 80%;
-            max-height: 450px;
+            width: 49vw;
+            height: 42vw;
         }
+    }
+
+
+    &__title {
+       @include centerPoint;
+       color: #431848;
+       text-shadow: -3px 2px black;
+       font-family: 'Schoolbell', cursive;
+
+        text-align: center;
+        white-space: nowrap;
+
+        @media (orientation: portrait) {
+           @include placeAbsolute(50%, 70%, false, false);
+           font-size: 14vw;
+        }
+
+        @media (orientation: landscape) {
+           @include placeAbsolute(50%, 70%, false, false);
+           font-size: 7vw;
+        }
+    }
+
+
+    &__picture {
+        background-image:url(./thumbsup.jpg);
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height:100%;      
+        z-index: -1;
+        filter: sepia(1);
+        margin: 0;
+        padding: 0;
     }
 }
 
