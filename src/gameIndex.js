87fc294aa0ca84169b4655dccd5d80f2ea34a0aa
dblatcher <dv.bltchr@gmail.com>
2020-02-13 21:37:37 +0000

    sierra version of interactions

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -1,12 +1,17 @@
 import TitleScreen from './steamed-hams/Title-Screen'
 import EndingScreen from './steamed-hams/Ending-Screen'
-import { interactionMatrix  } from './steamed-hams/game-interactions'
+import * as ScummMatrix from './steamed-hams/game-interactions'
+import * as SierraMatrix from './steamed-hams/game-interactions-sierra'
+
 import { makeConversations } from "./steamed-hams/game-conversations";
 import * as gameDataImport from "./steamed-hams/game-data";
 import * as gameConfigImport from './steamed-hams/game-config'
 
 import sequences from "./steamed-hams/sequences";
 
+//console.log(window.location)
+
+const {interactionMatrix} = true ? SierraMatrix : ScummMatrix
 
 const gameData = { ...gameDataImport, ...gameConfigImport,
     sequences, interactionMatrix, makeConversations,}
