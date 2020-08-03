bd3f490643ade83a2d104ebbe90749f2b56d9ceb
David Blatcher <david.blatcher@graduate-jobs.com>
2020-08-03 08:37:46 +0100

    install vuex, set up basic store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
-
 const gameIndex = require(`../games/${process.env.VUE_APP_GAME_NAME}/gameIndex`)
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    gameData:gameIndex.gameData,
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    },
+  }
+})
+
+
 
 Vue.config.productionTip = false
 
@@ -9,6 +25,7 @@ function launchApp(selector, rootProps) {
 
   return new Vue({
       el: selector,
+      store: store,
       data() {return {rootProps}},
       render(h) { 
           return h(App, {props: {
