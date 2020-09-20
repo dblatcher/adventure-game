59734d4128c4552e23d5116812bc4d556d9a552e
David Blatcher <david.blatcher@graduate-jobs.com>
2020-09-20 13:16:41 +0100

    formatting - CommandLine

--- src/components/CommandLine.vue
+++ src/components/CommandLine.vue
@@ -1,105 +1,106 @@
 <template>
-	<section class="command-line" 
-	v-bind:class="{
-		 disabled:disabled
-	}">
-	
-		<p v-show="!lastCommand.inProgress" 
-		class="command-line__sentence">
-			{{choosenText}}<span class="command-line__last-phrase">{{possibleText}}</span>
-		</p>
-		<p v-show="lastCommand.inProgress" 
-		class="command-line__sentence command-line__sentence--in-progress">
-			{{commandInProgessText}}
-		</p>
-	
-	</section>
-	
+  <section class="command-line" v-bind:class="{
+         disabled:disabled
+    }">
+    <p v-show="!lastCommand.inProgress" class="command-line__sentence">
+      {{choosenText}}
+      <span class="command-line__last-phrase">{{possibleText}}</span>
+    </p>
+    <p
+      v-show="lastCommand.inProgress"
+      class="command-line__sentence command-line__sentence--in-progress"
+    >{{commandInProgessText}}</p>
+  </section>
 </template>
 
 <script>
-
 function describe(thing) {
-return thing.quantified && thing.quantity !== 1 ? 
-	thing.quantity + ' ' +thing.pluralName : 
-	thing.name;
+  return thing.quantified && thing.quantity !== 1
+    ? thing.quantity + " " + thing.pluralName
+    : thing.name;
 }
 
 export default {
-	name:'CommandLine',
+  name: "CommandLine",
 
-	props:['needObject','disabled', 'verb','subject','object','thingHoveredOn','lastCommand'],
-	
-	computed: {
-		choosenText : function () {
+  props: [
+    "needObject",
+    "disabled",
+    "verb",
+    "subject",
+    "object",
+    "thingHoveredOn",
+    "lastCommand",
+  ],
 
-			var sentence = this.verb.description + ' ';
-			if (this.subject) {
-				sentence += describe(this.subject) + ' ';
-				if (this.needObject) {sentence += this.verb.preposition + ' ';}
-			}
-			if (this.object) {
-				sentence +=  describe(this.object);
-			}
-			
-			return sentence;
-		},
-		possibleText : function () {
-			return  this.thingHoveredOn ? describe(this.thingHoveredOn) : '';
-		},
-		commandInProgessText: function() {
-			const {verb, subject, object, inProgress} = this.lastCommand
-			if (!inProgress) { return ''}
-			let sentence = verb.description + ' ';
-			if (subject) {
-				sentence += describe(subject) + ' ';
-			}
-			if (object) {
-				sentence +=  verb.preposition + ' '
-				sentence +=  describe(object);
-			}
-			return sentence
-		}
-	},
+  computed: {
+    choosenText: function () {
+      var sentence = this.verb.description + " ";
+      if (this.subject) {
+        sentence += describe(this.subject) + " ";
+        if (this.needObject) {
+          sentence += this.verb.preposition + " ";
+        }
+      }
+      if (this.object) {
+        sentence += describe(this.object);
+      }
 
-}
+      return sentence;
+    },
+    possibleText: function () {
+      return this.thingHoveredOn ? describe(this.thingHoveredOn) : "";
+    },
+    commandInProgessText: function () {
+      const { verb, subject, object, inProgress } = this.lastCommand;
+      if (!inProgress) {
+        return "";
+      }
+      let sentence = verb.description + " ";
+      if (subject) {
+        sentence += describe(subject) + " ";
+      }
+      if (object) {
+        sentence += verb.preposition + " ";
+        sentence += describe(object);
+      }
+      return sentence;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+.command-line {
+  margin: 0 0.25rem 0.25rem;
+  background-color: black;
+  display: flex;
+  flex-wrap: wrap;
+  transition: background-color 0.75s, opacity 0.75s;
 
-	.command-line {
-		margin: 0 .25rem .25rem;
-		background-color: black;
-		display: flex;
-		flex-wrap: wrap;
-		transition: background-color 0.75s, opacity 0.75s;
+  &--complete {
+    background-color: yellow;
+  }
 
-		&--complete {
-			background-color:yellow;
-		}
+  &__sentence {
+    min-height: 1.2em;
+    margin: 0;
+    padding: 0.25rem 1rem;
+    color: white;
 
-		&__sentence {
-			min-height: 1.2em;
-			margin: 0;
-			padding: .25rem 1rem;
-			color:white;
+    &--in-progress {
+      color: red;
+    }
+  }
 
-			&--in-progress {
-				color: red;
-			}
-		}
-
-		&__last-phrase {
-			color: yellow;
-			@media (pointer: coarse) {
-				display: none;
-			}
-			@media (hover: none) {
-				display: none;
-			}
-		}
-
-
-	}
-
+  &__last-phrase {
+    color: yellow;
+    @media (pointer: coarse) {
+      display: none;
+    }
+    @media (hover: none) {
+      display: none;
+    }
+  }
+}
 </style>
